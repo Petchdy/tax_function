d12606cf458787cacf12da610205b9b43f89996f
Petchdy <skulwisit@gmail.com>
2020-04-24 13:23:13 +0700

    yy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     static double tax(int x){
-        double y;
+
         if (x>150_000){
             if (x>=5_000_001){
-                return y=x*0.35;
+                return x*0.35;
             }
             else if (x>=2_000_001){
-                return y=x*0.30;
+                return x*0.30;
             }
             else if (x>=1_000_001){
-                return y=x*0.25;
+                return x*0.25;
             }
             else if (x>=750_001){
-                return y=x*0.20;
+                return x*0.20;
             }
             else if (x>=500_001){
-                return y=x*0.15;
+                return x*0.15;
             }
             else if (x>=300_001){
-                return y=x*0.1;
+                return x*0.1;
             }
             else {
-                return y=x*0.05;
+                return x*0.05;
             }
         }
         else {
-            return y=0;
+            return x=0;
         }
     }
     public static void main(String[] args) {
